d251677ba19dd267c5d2f1088a1fb4bc001f1191
Andrew Osborn <andrewosborn93@gmail.com>
2026-01-05 12:42:21 -0700

    Use connection pool

--- nextjs/app/lib/db.ts
+++ nextjs/app/lib/db.ts
@@ -1,11 +1,14 @@
-import {Client} from 'pg';
+import {Pool} from 'pg';
 
-const dbClient = new Client({
+const dbClient = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT as string),
   database: process.env.DB_NAME,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 await dbClient.connect();
