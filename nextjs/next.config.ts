3f04dbb5b38acd824c4c2a318bfb8957de29e339
Andrew Osborn <andrewosborn93@gmail.com>
2026-01-05 23:36:51 -0700

    Hardcode the map

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  basePath: process.env.NEXT_BASE_PATH || '',
+  basePath: '/map',
 };
 
 export default nextConfig;
