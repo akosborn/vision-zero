4eadf6d4f9ca14e26f31bbf8bdc4cd0b942156f0
bcbeta <david.mintzer@gmail.com>
2026-08-26 21:23:09 -0600

    fix: correct vulnerable road user counts

--- nextjs/app/components/LocationReport/utils/location-report.ts
+++ nextjs/app/components/LocationReport/utils/location-report.ts
@@ -149,7 +149,7 @@ const getVulnerableRoadUserCounts = (crash: Crash): Record<Mode, number> => {
       ),
     );
 
-    let bikesInvolved = 0;
+    let bikesInvolved = bikeIndicatedTypes.length;
 
     if (bikeIndicatedTypes.length === 0) {
       // Sometimes the `mhe` column is populated with an indicator, but the `tu_1_nm_type` and `tu_2_nm_type` columns are empty
@@ -160,7 +160,7 @@ const getVulnerableRoadUserCounts = (crash: Crash): Record<Mode, number> => {
         bikesInvolved = 1;
       }
     }
-    if (bikeIndicatedTypes.length > 0) {
+    if (bikesInvolved > 0) {
       return {
         bicycle: bikesInvolved,
         pedestrian: 0,
@@ -176,12 +176,12 @@ const getVulnerableRoadUserCounts = (crash: Crash): Record<Mode, number> => {
       crash.cdot_tu_1_nm_type,
       crash.cdot_tu_2_nm_type,
     ].filter((value) =>
-      BIKE_INDICATORS.some((indicator) =>
+      PEDESTRIAN_INDICATORS.some((indicator) =>
         value?.toLowerCase().includes(indicator),
       ),
     );
 
-    let pedestriansInvolved = 0;
+    let pedestriansInvolved = pedestrianIndicatedTypes.length;
 
     if (pedestrianIndicatedTypes.length === 0) {
       // Sometimes the `mhe` column is populated with an indicator, but the `tu_1_nm_type` and `tu_2_nm_type` columns are empty
